9b2b4db8befffe0fd9c3eb21030e5a4811d31262
Lesley Park <lesparkley@gmail.com>
2016-12-12 05:45:54 +0000

    Edit/Destroy secured

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,12 +1,22 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    if @gram.user != current_user
+      return render text: 'Forbidden :(', status: :forbidden
+    end
+    @gram.destroy
+    redirect_to root_path
+  end
+
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
-    
     if @gram.valid?
       redirect_to root_path
     else
@@ -29,6 +39,7 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def create
@@ -46,8 +57,8 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render text: 'Not Found :(', status: :not_found
+  def render_not_found(status=:not_found)
+    render text: "#{status.to_s.titleize} :(", status: status
   end
 
 end

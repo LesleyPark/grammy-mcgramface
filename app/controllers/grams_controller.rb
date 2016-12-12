4058d06b7e23f18af955808937875346b028c4a7
Lesley Park <lesparkley@gmail.com>
2016-12-12 04:37:46 +0000

    Connect grams to users

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,19 +1,20 @@
 class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def new
     @gram = Gram.new
-    if @gram.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
   
   def index
   end
 
   def create
-    @gram = Gram.create(gram_params)
-    redirect_to root_path
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
